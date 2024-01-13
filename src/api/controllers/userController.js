7983b00793c208958a48cb8b3b6574f4e7fd0d6e
camarab <bafode.camara@sacem.fr>
2024-01-13 14:21:07 +0100

    authentifications: login register crud operation for user getAllsuer getoneuser update one user delete one user get logged user update user profile

---
+++ src/api/controllers/userController.js
@@ -0,0 +1,144 @@
+const asyncHandler=require('express-async-handler')
+const generateToken=require('../utils/generateToken.js')
+const User=require('../models/userModel.js')
+
+
+
+exports.login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body
+
+  const user = await User.findOne({ email })
+
+  if (user && (await user.matchPassword(password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id), 
+    })
+  } else {
+    res.status(401).json({message: 'Invalid email or password'})
+  }
+})
+
+
+exports.registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400).json({message: 'User already exists'})
+  }
+
+  const user = await User.create({ 
+    name,
+    email,
+    password,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(400).json({message: 'Invalid user data'})
+  }
+})
+
+
+exports.getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+exports.updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    if (req.body.password) {
+      user.password = req.body.password
+    }
+
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+exports.getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({})
+  res.json(users)
+})
+
+exports.deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+
+  if (user) {
+    await User.findByIdAndDelete(req.params.id)
+    res.json({ message: 'User removed' })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+exports.getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password')
+
+  if (user) {
+    res.json(user)
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+exports.updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const dotenv=require('dotenv')
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+
 
 const hostname = "0.0.0.0";
 let port = 3000
@@ -8,6 +11,48 @@ let port = 3000
 dotenv.config()
 
 const server = express();
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://mongo/music-api-node");
+
+server.use(express.json());
+server.use(express.urlencoded({extended:true}));
+
+const userRoute = require("./api/routes/userRoute");
+userRoute(server);
+
+const swaggerOptions = {
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'NODEJS API',
+        version: '1.0.0',
+        description: 'A sample API for learning nodejs',
+      },
+      components: {
+        securityDefinitions: {
+            bearerAuth: {
+                type: 'apiKey',
+                name: 'Authorization',
+                scheme: 'bearer',
+                in: 'header',
+            },
+        }
+      },
+      security: {
+        bearerAuth: [],
+      },
+      servers: [
+        {
+          url: 'http://localhost:3000',
+        },
+      ],
+    },
+    apis: ['./api/routes/*.js'],
+  };
+  
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 server.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
